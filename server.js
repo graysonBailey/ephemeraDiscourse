a163c0d2ff247a40acf8e022f7a2d7080b98c5a0
grayson bailey <47793096+graysonBailey@users.noreply.github.com>
2023-09-15 15:48:55 +0200

    init

---
+++ server.js
@@ -0,0 +1,128 @@
+const express = require('express')
+const Datastore = require('nedb')
+const socket = require('socket.io')
+const fetch = require('node-fetch')
+const path = require('path')
+
+
+var app = express();
+app.use(express.static('dist'));
+var server = app.listen(80);
+var io = socket(server);
+console.log("It's working! It's working!");
+
+var entire = new Datastore({
+  filename: 'ephemera.db',
+  autoload: true
+});
+
+// entire.remove({ $not: { db: 'metaDiscourse' }}, { multi: true }, function (err, numRemoved) {
+// });
+
+
+// entire.find({}, (err, docs) => {
+//   if (err) {
+//     console.log("couldn't change database...")
+//     response.end();
+//     return;
+//   }
+//
+// console.log(docs.length)
+//
+// for(let each in docs){
+//   if(docs[each].p.y > 8000){
+//     let thisU = docs[each].u
+//     let nextY = docs[each].p.y -4000
+//     let nextX = docs[each].p.x
+//     let newPos = {x:nextX,y:nextY}
+//
+//     entire.update({ u: thisU }, { $set: { p: newPos } }, {}, function (){})
+//
+//   }
+//
+//
+//
+// }
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+io.on('connection', newConnection);
+
+app.get('/:id',(req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/sepPrint.html'));
+});
+
+app.get('/sets/:id', (req,res) => {
+  entire.find({db: req.params.id}, (err, docs) => {
+    if (err) {
+      console.log("error in retrieval find process...")
+      response.end();
+      return;
+    }
+    console.log("grabbed it for a print job")
+    res.send(docs)
+  })
+})
+
+app.get('/aTE/all', (req,res) => {
+  entire.find({}, (err, docs) => {
+    if (err) {
+      console.log("error in retrieval find process...")
+      response.end();
+      return;
+    }
+    console.log("grabbed it for a print job")
+    res.send(docs)
+  })
+})
+
+
+
+
+
+function newConnection(socket) {
+
+  console.log("a new connection!")
+
+  socket.on('unit', data => {
+    socket.broadcast.emit('unit', data)
+    entire.insert(data);
+  })
+
+  socket.on('relation', data => {
+      entire.update({ u: data.u }, { $push: { r: data.r } }, {}, function (){})
+  })
+
+  socket.on('mouse', data => {
+    socket.broadcast.emit('mouseRep', data)
+  });
+
+  socket.on('gimmeData', data => {
+    entire.find({}, (err, docs) => {
+      if (err) {
+        console.log("error in retrieval find process...")
+        response.end();
+        return;
+      }
+      console.log("it got grabbed")
+      socket.emit('dataRep', docs)
+    })
+  });
+}
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+    resolve: {
+        fallback: {
+        "fs": false,
+        "tls": false,
+        "net": false,
+        "path": false,
+        "zlib": false,
+        "http": false,
+        "https": false,
+        "stream": false,
+        "crypto": false,
+  } 
+},
+  entry: {
+    main :'./src/index.js',
+    side: './src/print.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules:[
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+    mode: 'production',
+};
